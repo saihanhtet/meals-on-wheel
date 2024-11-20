e2ebcfb6c5cd4ee9d466d2bfe8fa835bc2a7cbd3
cofee <m2air@M2Airs-MacBook-Air.local>
2024-11-20 14:12:01 +0700

    setup frontend

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// view -> it is for the blade php pages
+// inertia -> it is for the react routes pages
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Home');
 });
